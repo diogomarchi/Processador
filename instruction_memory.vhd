8efdc7e4b54ceb75825de7b5fbb27df68cefce47
georgenardes <georgenardes@gmail.com>
2020-06-27 18:32:51 -0300

    ajustado memoria para n atrasar instrução

--- instruction_memory.vhd
+++ instruction_memory.vhd
@@ -15,9 +15,7 @@ use ieee.STD_LOGIC_UNSIGNED.all;
 entity instruction_memory is 
   generic (D_Width : integer := 16; -- data width
            A_Width : integer := 16); -- address width
-  port ( i_CLK   : in std_logic;       
-       i_CLR_n   : in std_logic;   
-       i_IR_RD   : in  std_logic;  -- enable read         		 
+  port(i_IR_RD   : in  std_logic;  -- enable read         		 
 		 i_IR_ADDR : in  std_logic_Vector(A_Width-1 downto 0); -- address
        o_R_DATA  : out  std_logic_Vector(D_Width-1 downto 0)); -- data output
 
@@ -35,29 +33,15 @@ architecture rtl of instruction_memory is
 		q		: out std_logic_vector(15 downto 0));	
   end component single_port_rom;
   
-  component register_16bit is
-  port ( i_CLK   : in std_logic;
-         i_CLR_N : in std_logic;
-         i_ENA   : in  std_logic;  -- enable
-         i_A     : in  std_logic_Vector(15 downto 0);  -- data input       
-         o_Q     : out  std_logic_Vector(15 downto 0)); -- data output
-  end component register_16bit;
-
 signal w_O_ROM: std_logic_vector(15 downto 0);
   
 begin  
-  	  
-  u_register_16bit: register_16bit port map( 
-			i_CLK   => i_CLK,
-         i_CLR_N => i_CLR_n,
-         i_ENA   => i_IR_RD,
-         i_A     => w_O_ROM,
-         o_Q     => o_R_DATA);
-			
-			
+  	
   u_single_port_rom: single_port_rom port map(
       addr	=> i_IR_ADDR,	   
 	   q		=> w_O_ROM);
+		
+  o_R_DATA <= w_O_ROM when (i_IR_RD = '1') else "ZZZZZZZZZZZZZZZZ";
 
 end rtl;
 
